bfee80d2a19ce9825027add1b861763673f031e5
JohnnysavageNI <johnnysavagedesign@gmail.com>
2025-05-18 15:39:28 +0100

    Add long in function

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Movie, Review, Comment
+from .forms import ReviewForm
 
 # Create your views here.
 
@@ -20,4 +23,18 @@ class MovieDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['reviews'] = Review.objects.filter(movie=self.object)
+        context['form'] = ReviewForm()
         return context
+
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.movie = self.object
+            review.user = request.user
+            review.save()
+            return redirect('movie_detail', pk=self.object.pk)
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
